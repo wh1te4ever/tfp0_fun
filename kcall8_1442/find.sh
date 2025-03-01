8f7f37bf42be73d62aed717596f8aeee094e3382
wh1te4ever <shg1725@nate.com>
2025-03-01 21:52:28 +0900

    Add some gadgets

--- kcall8_1442/find.sh
+++ kcall8_1442/find.sh
@@ -8,5 +8,8 @@ grep 'stp x[^;]*; stp x[^;]*; add[^;]*; mov[^;]*; ldr[^;]*; ldr[^;]*; ldr[^;]*; 
 # ldp[^;]*; ldp[^;]*; ldr[^;]*; mov[^;]*; br
 grep 'ldp[^;]*; ldp[^;]*; ldr[^;]*; mov[^;]*; br' kernelcache-gadgets.txt > populate.txt
 
+# mov x?, x? ; br x?
+grep -E '\bmov[[:space:]]+x[0-9]+,[[:space:]]*x[0-9]+ ; br x' kernelcache-gadgets.txt > mov_br.txt
+
 # ldp x1, x2, [^;]*; ldp x3, x4, [^;]*; [^;]*; [^;]*; [^;]*; [^;]*; [^;]*;[^;]*;
 # ^(?!.*\bsp\b).*ldp x1, x2, [^;]*; ldp x3, x4, [^;]*;
